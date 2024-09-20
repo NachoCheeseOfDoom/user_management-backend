bbf194ed89769ae230f01985aceb77e6781aa967
NachoCheeseOfDoom <85528067+NachoCheeseOfDoom@users.noreply.github.com>
2024-09-20 14:52:59 -0600

    Update cors.js

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -1,12 +1,10 @@
-// const cors = require("cors");
+const cors = require("cors");
 
-// const corsOptions = {
-//     origin: ["https://fascinating-capybara-117da0.netlify.app/", "http://localhost:5173"],
-//     methods: ["GET", "POST", "DELETE", "PUT"],
-//     credentials: true,
-// };
+const corsOptions = {
+    origin: "https://fascinating-capybara-117da0.netlify.app",
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+};
 
-const cors = require('cors');
-app.use(cors({ origin: "https://fascinating-capybara-117da0.netlify.app" }));
 
-// module.exports = cors(corsOptions);
+module.exports = cors(corsOptions);
