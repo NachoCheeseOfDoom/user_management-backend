377e05c96676576ba1e4492111b78d2789eaa7d6
NachoCheeseOfDoom <85528067+NachoCheeseOfDoom@users.noreply.github.com>
2024-09-19 21:03:27 -0600

    updated the db connection

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,9 @@
 const mysql = require('mysql2');
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
+
+const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}:
+${process.env.MYSQLHOST}:${process.env.MYSQLPORT}:${process.env.MYSQLDATABASE}:`
+
+const connection = mysql.createConnection(urlDB);
 
 connection.connect(err => {
     if (err) {
