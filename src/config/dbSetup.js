ab52ceabd675362309ad9157c3f24abe6ce24b58
NachoCheeseOfDoom <85528067+NachoCheeseOfDoom@users.noreply.github.com>
2024-09-21 15:23:41 -0600

    data base query

---
+++ src/config/dbSetup.js
@@ -0,0 +1,15 @@
+const connection = require('./db'); // Import the db connection from db.js
+
+const query = `
+  ALTER TABLE users
+  ADD CONSTRAINT unique_email UNIQUE (email);
+`;
+
+connection.query(query, (err, results) => {
+    if (err) {
+        console.error('Error adding unique constraint to email field:', err);
+    } else {
+        console.log('Unique constraint added to email field successfully.');
+    }
+    connection.end(); // Close the connection after the query
+});
