18732ecd58dce0abc4178d1746bf9fe922d2629f
NachoCheeseOfDoom <85528067+NachoCheeseOfDoom@users.noreply.github.com>
2024-09-21 16:03:39 -0600

    removed db modification

--- src/server.js
+++ src/server.js
@@ -4,25 +4,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
-const connection = require('./config/db');
-
-connection.query('ALTER TABLE users MODIFY email VARCHAR(255)', (err) => {
-    if (err) {
-        console.error('Error modifying email column: ', err);
-    } else {
-        console.log('Email column modified to VARCHAR(255)');
-
-        // Now add the unique constraint
-        connection.query('ALTER TABLE users ADD CONSTRAINT unique_email UNIQUE (email)', (err) => {
-            if (err) {
-                console.error('Error adding unique constraint: ', err);
-            } else {
-                console.log('Unique constraint added to the email field');
-            }
-        });
-    }
-});
-
 
 
 const app = express();
